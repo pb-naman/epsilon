24878639a0c20b5a49221d8198cf7f7e9042ada6
pb-naman <naman.chawhan@pinboardconsulting.com>
2024-09-18 17:09:03 +0100

    backend working

--- app/main.py
+++ app/main.py
@@ -1,17 +1,26 @@
 from typing import Any, Dict, AnyStr, List, Union
-from fastapi import Request, FastAPI
+from fastapi import Request, FastAPI, HTTPException
 from pydantic import BaseModel
-from credentials import *
+from utils import tg_runInstalledQuery
+import json
 
 app = FastAPI()
 
+class Item(BaseModel):
+    queryname: str
+    params: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/runinstalledquery/")
-async def run_installed_query(request: Request):
-    return await request.json()
-
-
+def run_installed_query(item: Item):
+    if item.queryname == "" or item.params == "":
+        raise HTTPException(status_code=400, detail="query name or query parameters missing")
+    try:
+        params = json.loads(item.params)
+    except:
+        raise HTTPException(status_code=400, detail="invalid query parameters")
+    query_output = tg_runInstalledQuery(item.queryname, params)
+    return query_output
---
+++ app/utils.py
@@ -0,0 +1,16 @@
+import pyTigerGraph as tg
+from credentials import *
+
+def get_tg_conn():
+	conn = tg.TigerGraphConnection(host=HOST,
+		username=USERNAME,
+		password=PASSWORD,
+		graphname=GRAPHNAME
+		)
+	conn.apiToken = conn.getToken(Secret)
+	return conn
+
+
+def tg_runInstalledQuery(queryname, params):
+	conn_obj = get_tg_conn()
+	return conn_obj.runInstalledQuery(queryName = queryname, params = params)
